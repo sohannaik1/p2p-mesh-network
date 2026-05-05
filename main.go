9386ee0aceaf5236a4102ae8b77a265ddf152922
SohanNaik1 <sohannaik059@gmail.com>
2026-05-06 00:20:39 +0530

    Changed postion of listner. Also can now find peers and send hello messages.(integrated mDNS discovery with TCP transport)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 
@@ -12,23 +13,6 @@ func main() {
 	service, _ := mdns.NewMDNSService("Sohan-Arch", "_p2p-mesh._tcp", "", "", 9876, nil, []string{"Version=0.1"})
 	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
 	defer server.Shutdown()
-
-	fmt.Println("mDNS Server started. Looking for peers...")
-
-	// setup client
-	// Create a channel to receive found entries
-	entriesCh := make(chan *mdns.ServiceEntry, 10)
-
-	// Start a goroutine to print whatever the channel finds
-	go func() {
-		for entry := range entriesCh {
-			fmt.Printf("Found Peer! Name: %s | IP: %v | Port: %d\n", entry.Name, entry.AddrV4, entry.Port)
-		}
-	}()
-
-	// start lookup
-	// This tells the library: "Search for anyone using our protocol"
-	mdns.Lookup("_p2p-mesh._tcp", entriesCh)
 
 	ln, err := net.Listen("tcp", ":9876")
 	if err != nil {
@@ -42,9 +26,36 @@ func main() {
 				continue
 			}
 			fmt.Println("New peer connected from:", conn.RemoteAddr())
-			conn.Close()
+			message, _ := bufio.NewReader(conn).ReadString('\n')
+			fmt.Print("Message received : ", message)
 		}
 	}()
+
+	fmt.Println("mDNS Server started. Looking for peers...")
+
+	// setup client
+	// Create a channel to receive found entries
+	entriesCh := make(chan *mdns.ServiceEntry, 10)
+
+	// Start a goroutine to print whatever the channel finds
+	go func() {
+		for entry := range entriesCh {
+			fmt.Printf("Found Peer! Name: %s | IP: %v | Port: %d\n", entry.Name, entry.AddrV4, entry.Port)
+			address := fmt.Sprintf("%s:%d", entry.AddrV4, entry.Port)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				print("Dial failed: ", err)
+				continue
+			}
+			fmt.Fprintf(conn, "Hello form Sohan-Arch\n")
+			defer conn.Close()
+		}
+	}()
+
+	// start lookup
+	// This tells the library: "Search for anyone using our protocol"
+	mdns.Lookup("_p2p-mesh._tcp", entriesCh)
+
 	// keep program alive
 	select {}
 }
