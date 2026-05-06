c1b489b35f6fb9e59c1a5afd8c0f96ade5c94402
SohanNaik1 <sohannaik059@gmail.com>
2026-05-06 22:59:04 +0530

    Created an actual 'mesh network' where multiple peers can connect to each other and send messages.

--- main.go
+++ main.go
@@ -2,22 +2,45 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
+	"strings"
+	"sync"
 
 	"github.com/hashicorp/mdns"
 )
 
+type PeerInfo struct {
+	Name string `json:"name"`
+	Ip   string `json:"ip"`
+	Port int    `json:"port"`
+}
+
+var (
+	peers = make(map[string]PeerInfo)
+	mu    sync.Mutex
+
+	namePtr *string
+	portPtr *int
+)
+
 func main() {
+	// Setup Flags
+	namePtr = flag.String("name", "Sohan-Arch", "Name of the peer")
+	portPtr = flag.Int("port", 9876, "Port to listen on")
+	flag.Parse()
+
 	// setup server
-	service, _ := mdns.NewMDNSService("Sohan-Arch", "_p2p-mesh._tcp", "", "", 9876, nil, []string{"Version=0.1"})
+	service, _ := mdns.NewMDNSService(*namePtr, "_p2p-mesh._tcp", "", "", *portPtr, nil, []string{"Version=0.1"})
 	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
 	defer server.Shutdown()
 
-	ln, err := net.Listen("tcp", ":9876")
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", *portPtr))
 	if err != nil {
 		panic(err)
 	}
+
 	go func() {
 		for {
 			conn, err := ln.Accept()
@@ -28,10 +51,11 @@ func main() {
 			fmt.Println("New peer connected from:", conn.RemoteAddr())
 			message, _ := bufio.NewReader(conn).ReadString('\n')
 			fmt.Print("Message received : ", message)
+			conn.Close()
 		}
 	}()
 
-	fmt.Println("mDNS Server started. Looking for peers...")
+	fmt.Printf(">>> Node [%s] started on port %d <<<\n", *namePtr, *portPtr)
 
 	// setup client
 	// Create a channel to receive found entries
@@ -40,15 +64,37 @@ func main() {
 	// Start a goroutine to print whatever the channel finds
 	go func() {
 		for entry := range entriesCh {
-			func(e *mdns.ServiceEntry) {
-				address := fmt.Sprintf("%s:%d", e.AddrV4, e.Port)
-				conn, err := net.Dial("tcp", address)
-				if err != nil {
-					return
+			// a. Skip my own machine
+			if strings.HasPrefix(entry.Name, *namePtr) {
+				fmt.Println("Skipping myself")
+				continue
+			}
+			// b. Lock the map until we write/check
+			mu.Lock()
+			_, alreadyExists := peers[entry.Name]
+
+			if !alreadyExists {
+				// c. The new entry is saved in the map if it already does not exist in map.
+				peers[entry.Name] = PeerInfo{
+					Name: entry.Name,
+					Ip:   entry.AddrV4.String(),
+					Port: entry.Port,
 				}
-				defer conn.Close()
-				fmt.Fprintf(conn, "Hello form Sohan-Arch\n")
-			}(entry)
+				mu.Unlock() // Unlock after writing the map
+
+				// d. Dialing
+				go func(e *mdns.ServiceEntry) {
+					address := fmt.Sprintf("%s:%d", e.AddrV4, e.Port)
+					conn, err := net.Dial("tcp", address)
+					if err != nil {
+						return
+					}
+					fmt.Fprintf(conn, "Hello from %s\n", *namePtr)
+					conn.Close()
+				}(entry)
+			} else {
+				mu.Unlock() // Unlock map if we aleady know the peer.
+			}
 		}
 	}()
 
